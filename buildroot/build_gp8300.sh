65d0bda25a00f20019ebe78650ccf1e33e6d9df4
junhui.zhang <zhangjunhui787@163.com>
2021-04-30 10:48:06 +0800

    origin version: buildroot_base_v202102

---
+++ buildroot/build_gp8300.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SHELL_FILE=$(readlink -f $0)
+BUILDROOT_ROOT=$(dirname $SHELL_FILE)
+BUILD_DIR=$ROOTFS_ROOT/build
+LOCAL_TOOLCHAIN_PATH=$(which gpt-gcc)
+export gpt_ver=v6.5
+
+sub_dir=${LOCAL_TOOLCHAIN_PATH%/*}
+dir=${sub_dir%/*}
+#cd $BUILD_DIR"/"$BUILDROOT_DIR
+#sed -i "s#gpt_toolchain_path#$dir#g" ./configs/gpt_polaris_dvb_mini_tools_defconfig
+
+make -j8
