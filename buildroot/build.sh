5a5e21354d73026dc35dc862b7307e9445813dcc
zjh <zhangjunhui787@163.com>
2021-05-18 10:59:13 +0800

    modify build.sh

--- buildroot/build.sh
+++ buildroot/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-KO_DIR=$PWD/../user_ko
+KO_DIR=$PWD/../KO
 KERNEL_DIR=$PWD/output/build/linux-custom
 
 make ARCH=gpt clean
 make ARCH=gpt
+echo "========== build success! ============"
 
+echo "========== Move KO file =========="
 cd $KERNEL_DIR
 find -name *.ko > ko.txt
-count=1
 cat ko.txt | while read line
 do 
 	cp $line $KO_DIR
-	count=$[ $count + 1 ]
 done
 
 rm ko.txt
-echo "==========build success!============"
+echo "========== Move  KO file end! =========="
